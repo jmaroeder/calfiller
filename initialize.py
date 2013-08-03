7460e44c5d2946f19b1d6f2465361428e83ead6a
James Allen <james.m.allen@gmail.com>
2013-08-03 13:03:53 -0400

    Better Style (courtesy of WEE CSS)

---
+++ initialize.py
@@ -0,0 +1,13 @@
+from calfiller import *
+#import datetime, csv
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db.init_app(app)
+with app.test_request_context():
+	db.create_all()
+
+
+db.session.add(School(name='Hathaway Brown Upper School', short_name='hbus', password_hash=generate_password_hash('brown')))
+db.session.add(School(name='Hathaway Brown Middle School', short_name='hbms', password_hash=generate_password_hash('brown')))
+db.session.commit()
+
